acd63d47f3d25a8a77ba463406015942e146c8ac
Jameson Martins <jamesonmartins127@gmail.com>
2020-06-25 22:48:10 -0400

    Tabuleiro criado em forma crescente

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main()
+        {            
+            
+            int tamHorizontal, tamVertical;
+            do {
+                Console.WriteLine("TAMANHO DO TABULEIRO");
+                Console.Write("Horizontalmente: ");
+                tamHorizontal = int.Parse(Console.ReadLine());
+                Console.Write("Verticalmente: ");
+                tamVertical = int.Parse(Console.ReadLine());
+                if(tamHorizontal * tamVertical >= 100) {
+                    Console.WriteLine("Tamanho máximo ultrapassado");
+                }
+            } while (tamHorizontal * tamVertical >= 100);
+
+            Tabuleiro tabObj = new Tabuleiro(tamHorizontal, tamVertical);
+            int[,] tabuleiro = tabObj.criarTabuleiro();
+
+            for (int i = 0; i < tamVertical; i++) {
+                for (int j = 0; j < tamHorizontal; j++) {
+                    if (tabuleiro[i, j] < 10) {
+                        Console.Write("  " + tabuleiro[i, j] + " ");
+                    }
+                    else {
+                        Console.Write(" " + tabuleiro[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            
+        }
+    }
+}
+
+
+/*
+for (int i = 0; i < tamVertical; i++) {
+    for (int j = 0; j < tamHorizontal; j++) {
+        if (tabuleiro[i, j] < 10) {
+            Console.Write("  " + tabuleiro[i, j] + " ");
+        }
+        else {
+            Console.Write(" " + tabuleiro[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
+
+*/
---
+++ Tabuleiro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game
+{
+    class Tabuleiro
+    {
+        public int tamHor;
+        public int tamVer;
+
+        public Tabuleiro() { }
+        public Tabuleiro(int tamHorizontal, int tamVertical)
+        {
+            tamHor = tamHorizontal;
+            tamVer = tamVertical;
+        }
+
+        public int[,] criarTabuleiro()
+        {
+            int[,] tabArray = new int[tamVer, tamHor];                
+            int count = 1;
+            for (int i = 0; i < tamVer; i++) {
+                for (int j = 0; j < tamHor; j++) {
+                    tabArray[i, j] = count;
+                    count++;
+                }
+            }
+            return tabArray;
+        }
+    }
+}
