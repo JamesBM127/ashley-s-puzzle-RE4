3ad11da36d6a843c67db167483ddf1865060d622
Jameson Martins <jamesonmartins127@gmail.com>
2020-06-26 00:17:29 -0400

    Tabuleiro gerando numeros aleatorios que nao se repetem. OK

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 namespace game
 {
     class Program
     {
         static void Main()
-        {            
-            
+        {
             int tamHorizontal, tamVertical;
             do {
                 Console.WriteLine("TAMANHO DO TABULEIRO");
@@ -19,20 +19,8 @@ namespace game
                 }
             } while (tamHorizontal * tamVertical >= 100);
 
-            Tabuleiro tabObj = new Tabuleiro(tamHorizontal, tamVertical);
+            Tabuleiro tabObj = new Tabuleiro(tamHorizontal, tamVertical); //Criar o OBJ
             int[,] tabuleiro = tabObj.criarTabuleiro();
-
-            for (int i = 0; i < tamVertical; i++) {
-                for (int j = 0; j < tamHorizontal; j++) {
-                    if (tabuleiro[i, j] < 10) {
-                        Console.Write("  " + tabuleiro[i, j] + " ");
-                    }
-                    else {
-                        Console.Write(" " + tabuleiro[i, j] + " ");
-                    }
-                }
-                Console.WriteLine();
-            }
 
             
         }
--- Tabuleiro.cs
+++ Tabuleiro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace game
 {
@@ -18,15 +19,33 @@ namespace game
 
         public int[,] criarTabuleiro()
         {
-            int[,] tabArray = new int[tamVer, tamHor];                
-            int count = 1;
+            int[,] tabArray = new int[tamVer, tamHor];
+            Random random = new Random();
+            int maxValue = tamHor * tamVer;
             for (int i = 0; i < tamVer; i++) {
                 for (int j = 0; j < tamHor; j++) {
-                    tabArray[i, j] = count;
-                    count++;
+                    int aux = random.Next(1, maxValue + 1);
+                    if (ecxist(aux, tabArray) == false) {
+                        tabArray[i, j] = aux;
+                    }
+                    else {
+                        j--;
+                    }
                 }
             }
             return tabArray;
+        }
+
+        private bool ecxist(int numero, int[,]tabArray)
+        {
+            for(int i = 0; i < tamVer; i++) {
+                for(int j = 0; j < tamHor; j++) {
+                    if(tabArray[i,j] == numero) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
